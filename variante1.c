e4e2921df241c1e3d02a937ebc275998fb48da19
joao simoes <joao.simoes@tecnico.ulisboa.pt>
2023-10-01 00:44:42 +0100

    so inciar merdas

--- variante1.c
+++ variante1.c
@@ -6,21 +6,19 @@
 Matriz *variante1(Matriz *matrix)
 {
     Node *aux = matrix->head, *auxT;
-    //int encotrado = 0;
-    //int i = 0;
+    // int encotrado = 0;
+    // int i = 0;
 
     while (aux != NULL)
     {
-        if(aux->indice == matrix->cordX)
+        if (aux->indice == matrix->cordX)
             break;
-    
+
         auxT = aux->next;
         aux = auxT;
     }
 
-    //aux->data[matrix->cordY-1];
-    
-
+    // aux->data[matrix->cordY-1];
 
     return matrix;
 }
---
+++ variante2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "tileblaster.h"
+
+Matriz *GravidadeVertical(Matriz *matrix)
+{
+    int branco = 1;
+    Matriz *aux = matrix->head;
+    for (int i = 0; i == matrix->colu; i++)
+    {
+        aux = matrix->head;
+        while (aux != NULL)
+        {
+        }
+    }
+}
